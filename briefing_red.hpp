e8331ff03b21e6ef2d3f290e21dfba6fc79e6c47
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:47:12 +0200

    tb_rhs_spartan_forefathers.stratis mission added

---
+++ briefing_red.hpp
@@ -0,0 +1,9 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <RP> Rally Points<br />
+<*> Help out your team by deploying <FO> Forward Outposts<br />
+<br />
+
+<titleFont>Attacking Strategies:</font><br />
+<*> Establishing bases of fire and flanking the enemy will be important, the US forces have mutually supporting objectives and firing positions<br />
+<*> Utilize APCs . Their support will be most welcome!<br />
---
+++ tb_objectReplacements.hpp
@@ -0,0 +1,6 @@
+class cfgVehicles
+{
+	#include "factions\BLU_F_ammoBox.hpp"
+	#include "factions\OPF_F_ammoBox.hpp"
+	//#include "factions\OPF_G_F_ammoBox.hpp"
+};
